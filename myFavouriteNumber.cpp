d18f8d7635fc17a75fa988e8d47366068c12c0a9
Daisy Welham <96212072+DaisyWelham@users.noreply.github.com>
2025-12-26 15:30:47 +0000

    Add files via upload

---
+++ myFavouriteNumber.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int myFavouriteNumber = 67;
+    string myFav = to_string(myFavouriteNumber);
+    cout << "My favourite number is: " << myFav << "\n";
+}
